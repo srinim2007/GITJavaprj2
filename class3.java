f31e26bc3a0c16afe441a8de34f41b2c8b49dee9
srini <srinim.hyd@gmail.com>
2024-06-11 20:49:04 +0100

    Updted code to GIT

--- class3.java
+++
@@ -1,7 +0,0 @@
-public class class3{
-  int a = 10, b = 20;
-
-  void add(){
-    System.out.println("Addition of a and b is: "+(a+b));
-
-}
---
+++ src/class3.java
@@ -0,0 +1,9 @@
+public class class3 {
+  int a = 10, b = 20;
+
+  void add() {
+    System.out.println("Addition of a and b is: " + (a + b));
+
+  }
+
+}
