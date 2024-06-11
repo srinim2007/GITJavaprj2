c3a503f39eeddc67e185282d43435ac14436d364
srinim2007 <srinim.hyd@gmail.com>
2024-06-11 20:29:16 +0100

    Update Class2.java
    Class2 updated

--- src/Package1/Class2.java
+++ src/Package1/Class2.java
@@ -2,6 +2,7 @@ package Package1;
 public class class2 {
   public static void main(String[] args){
   System.out.println("Class2 is newly created from GIT directly");
+    System.out.println("Class2 is updated");    
     
   }
 
