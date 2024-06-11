edd67c18cc90ed6428e1fbf7b28faa8b712df792
srini <srinim.hyd@gmail.com>
2024-06-11 21:03:03 +0100

    code in Package2 and class5 added

---
+++ src/Package2/class5.java
@@ -0,0 +1,9 @@
+package Package2;
+
+public class class5 {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Package2 class5");
+    }
+}
