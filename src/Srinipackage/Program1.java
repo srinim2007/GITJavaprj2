990902fde7fee7129679e79b35cf8f2ea7c02a19
srini <srinim.hyd@gmail.com>
2024-06-11 21:35:11 +0100

    lastest change committed

---
+++ src/Srinipackage/Program1.java
@@ -0,0 +1,21 @@
+package Srinipackage;
+
+public class Program1 {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        int x = 10, y = 20;
+
+        int m;
+        m = 10;
+
+        String name = "Student1";
+
+//        System.out.println(20+30);
+        System.out.println(a+b);
+        System.out.println(name);
+
+    }
+}
+
